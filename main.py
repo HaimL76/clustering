b3408ca8a643a177917b801cfc5a748d6a099bdc
unknown <haiml76@yahoo.com>
2023-03-03 12:20:54 +0200

    41

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from image import cluster_image_implemented, cluster_image_with_lib
 
 #cluster_image_with_lib(r"""c:\html\kitniot1.jpg""", 86, display_optimal_k=True)
-cluster_image_implemented(r"""c:\html\kitniot1.jpg""", 3)
+cluster_image_implemented(r"""c:\html\kitniot1.jpg""", 6)
