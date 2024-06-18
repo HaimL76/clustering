7e7b7096a46d51fb1b8adf1db03556e9e3e268fc
auggie lavi <haiml76@yahoo.com>
2024-06-18 12:24:08 +0300

    compression d2

--- Compression/Compression/DictionaryTree.cs
+++ Compression/Compression/DictionaryTree.cs
@@ -8,6 +8,8 @@ namespace Compression
 {
     public class DictionaryTreeNode<T>
     {
+        private int numNodes;
+
         private DictionaryTreeNode<T>[] arr;
 
         private T val;
@@ -15,17 +17,27 @@ namespace Compression
         public void Add(T val0, params char[] path)
             => Add(val0, 0, path);
 
+        private static Func<char, int, int> HashKey = (ch, num) =>
+        {
+            if (num < 10)
+                return ch % 10;
+
+            return (int)ch;
+        };
+
         private void Add(T val0, int index, params char[] path)
         {
             if (index < path.Length)
             {
                 char ch = path[index];
 
+                int hash = HashKey(ch, numNodes);
+
                 int length = (arr?.Length).GetValueOrDefault();
 
-                if (ch >= length)
+                if (hash >= length)
                 {
-                    var arr0 = new DictionaryTreeNode<T>[ch + 1];
+                    var arr0 = new DictionaryTreeNode<T>[hash + 1];
 
                     for (int i = 0; i < length; i++)
                         arr0[i] = arr[i];
@@ -33,9 +45,25 @@ namespace Compression
                     arr = arr0;
                 }
 
-                arr[ch] = arr[ch] ?? new DictionaryTreeNode<T>();
+                int numNodes0 = numNodes;
 
-                arr[ch].Add(val0, index + 1, path);
+                int index0 = hash;
+
+                DictionaryTreeNode<T> node = null;
+
+                while (numNodes0 == numNodes && index0 < arr.Length)
+                {
+                    node = arr[index0];
+
+                    if (node == null)
+                    {
+                        node = arr[index0] = new DictionaryTreeNode<T>();
+
+                        numNodes++;
+                    }
+                }
+
+                node.Add(val0, index + 1, path);
             }
             else
             {
