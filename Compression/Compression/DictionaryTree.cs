579c158d7eb39f03d19835c133e20296cd1e4295
Haim Lavi <HaimL1@verifone.com>
2024-06-23 16:15:08 +0300

    compression d4

--- Compression/Compression/DictionaryTree.cs
+++ Compression/Compression/DictionaryTree.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Compression
 {
     public class DictionaryTreeNode<T>
     {
+        public DictionaryTreeNode(char keyPart0) => keyPart = keyPart0;
+
         private int numNodes;
 
         private DictionaryTreeNode<T>[] arr;
 
         private T val;
+
+        private char keyPart;
 
         public void Add(T val0, params char[] path)
             => Add(val0, 0, path);
@@ -22,8 +28,45 @@ namespace Compression
             if (num < 10)
                 return ch % 10;
 
-            return (int)ch;
+            return ch;
         };
+
+        private DictionaryTreeNode<T> PutFirstAvailablePlace(char keyPart0, int startingIndex)
+        {
+            int length = (arr?.Length).GetValueOrDefault();
+
+            if (length <= startingIndex)
+            {
+                var arr0 = new DictionaryTreeNode<T>[startingIndex + 1];
+
+                for (int i = 0; i < length; i++)
+                    arr0[i] = arr[i];
+
+                arr = arr0;
+            }
+
+            DictionaryTreeNode<T> result = null;
+
+            int counter = 0;
+
+            int index = startingIndex;
+
+            while (result == null && counter++ < arr.Length)
+            {
+                DictionaryTreeNode<T> node = arr[index];
+
+                if (node == null)
+                    result = arr[index] = new DictionaryTreeNode<T>(keyPart0);
+                else if (node.keyPart == keyPart0)
+                    result = node;
+
+                index++;
+
+                index %= arr.Length;
+            }
+
+            return result;
+        }
 
         private void Add(T val0, int index, params char[] path)
         {
@@ -33,47 +76,10 @@ namespace Compression
 
                 int hash = HashKey(ch, numNodes);
 
-                int length = (arr?.Length).GetValueOrDefault();
+                var node = PutFirstAvailablePlace(ch, hash);
 
-                if (hash >= length)
-                {
-                    var arr0 = new DictionaryTreeNode<T>[hash + 1];
-
-                    for (int i = 0; i < length; i++)
-                        arr0[i] = arr[i];
-
-                    arr = arr0;
-                }
-
-                int numNodes0 = numNodes;
-
-                int index0 = hash;
-
-                DictionaryTreeNode<T> node = null;
-
-                int counter = 0;
-
-                while (numNodes0 == numNodes && counter++ < arr.Length)
-                {
-                    node = arr[index0];
-
-                    if (node == null)
-                    {
-                        node = arr[index0] = new DictionaryTreeNode<T>();
-
-                        numNodes++;
-                    }
-
-                    index0++;
-
-                    if (index0 == arr.Length)
-                        index0 = 0;
-                }
-
-                if (numNodes0 == numNodes)
+                if (node == null)
                     _ = 0;
-
-                node.Add(val0, index + 1, path);
             }
             else
             {
@@ -84,7 +90,7 @@ namespace Compression
 
     public class DictionaryTree<T>
     {
-        private readonly DictionaryTreeNode<T> root = new DictionaryTreeNode<T>();
+        private readonly DictionaryTreeNode<T> root = new DictionaryTreeNode<T>('\0');
 
         public void Add(T val0, params char[] path)
             => root.Add(val0, path);
